08a67fbaf6b2ea64c8c12ddc2619437240f67579
Sarah <sarah.bloom@my.jcu.edu.au>
2018-08-20 13:16:09 +1000

    Add password entry program.

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,13 @@
 """Sarah"""
+
+MIN_LENGTH = 10
+
+password = input("Enter a password: ")
+
+if len(password) < MIN_LENGTH:
+    print("Invalid password!")
+    password = input("Enter a password: ")
+else:
+    for char in password:
+        print('*', end=' ')
+print()
